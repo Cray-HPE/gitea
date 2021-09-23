11073aded2f08ad80b31ca1fd7fb5def9a3a5702
Ryan Bak <rbak@cray.com>
2021-09-23 14:48:55 -0700

    CASMCMS-7546: Update to rootless container

--- kubernetes/gitea/files/setup.sh
+++ kubernetes/gitea/files/setup.sh
@@ -23,16 +23,18 @@
 
 # Create the admin user for gitea
 
-echo `date` >> /data/gitea/setup
-rm -f /data/git/.gitconfig.lock
+DATA_MOUNT=/var/lib/gitea
+LOG_FILE=${DATA_MOUNT}/setup.log
+echo `date` >> $LOG_FILE
+rm -f ${DATA_MOUNT}/git/.gitconfig.lock
 
 # Wait for the service to be up and running before attempting to run the cli
-echo "Waiting for gitea web service to be available..." >> /data/gitea/setup
+echo "Waiting for gitea web service to be available..." >> $LOG_FILE
 attempt_counter=0
 max_attempts=30
-until $(curl --output /data/gitea/curl --silent --head --fail http://localhost:3000); do
+until $(curl --output ${DATA_MOUNT}/curl.out --silent --fail http://localhost:3000); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
-      echo "Max attempts reached" >> /data/gitea/setup
+      echo "Max attempts reached" >> $LOG_FILE
       exit 1
     fi
     printf '.'
@@ -41,12 +43,12 @@ until $(curl --output /data/gitea/curl --silent --head --fail http://localhost:3
 done
 
 # Wait for secret mounts to be in place before attempting to run the cli
-echo "Waiting for vcs user credentials to be available..." >> /data/gitea/setup
+echo "Waiting for vcs user credentials to be available..." >> $LOG_FILE
 attempt_counter=0
 max_attempts=30
 until $([[ -f "/mnt/crayvcs-credentials/vcs_username" ]]); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
-      echo "Max attempts reached" >> /data/gitea/setup
+      echo "Max attempts reached" >> $LOG_FILE
       exit 1
     fi
     printf '.'
@@ -54,29 +56,36 @@ until $([[ -f "/mnt/crayvcs-credentials/vcs_username" ]]); do
     sleep 5
 done
 
+# The migration to the rootless container requires that hooks be regenerated
+if [ -f "/var/lib/gitea/regenerate-hooks" ]; then
+  echo "Regenerating git hooks" >> $LOG_FILE
+  /usr/local/bin/gitea admin regenerate hooks --config ${DATA_MOUNT}/app.ini
+  rm /var/lib/gitea/regenerate-hooks
+fi
+
 # Setting up crayvcs user, gitea has no way of listing users to check if the
 # user already exists, so have to create one and check the return code. Gitea
 # issue has been filed: https://github.com/go-gitea/gitea/issues/6001
-echo "Creating admin user" >> /data/gitea/setup
+echo "Creating admin user" >> $LOG_FILE
 CRAYVCS_USER=$(</mnt/crayvcs-credentials/vcs_username)
 CRAYVCS_PASSWORD=$(</mnt/crayvcs-credentials/vcs_password)
 CRAYVCS_USER_EMAIL="${CRAYVCS_USER}@mgmt-plane-nmn.local"
-cd /data/gitea
-echo "Running in `pwd`" >> /data/gitea/setup
-/app/gitea/gitea admin create-user \
-    --config /data/gitea/conf/app.ini \
-    --name ${CRAYVCS_USER} \
+cd ${DATA_MOUNT}/custom
+echo "Running in `pwd`" >> $LOG_FILE
+/usr/local/bin/gitea admin user create \
+    --config ${DATA_MOUNT}/app.ini \
+    --username ${CRAYVCS_USER} \
     --password ${CRAYVCS_PASSWORD} \
     --admin \
     --must-change-password=false \
-    --email "${CRAYVCS_USER_EMAIL}" &>> /data/gitea/setup
+    --email "${CRAYVCS_USER_EMAIL}" &>> $LOG_FILE
 
 RESULT=$?
 if [ $RESULT == 0 ]; then
   # Leave a breadcrumb that the user has been created
-  echo "admin user '${CRAYVCS_USER}' created" >> /data/gitea/setup
-  touch /data/gitea/crayvcs_user_created
-  echo `date` >> /data/gitea/crayvcs_user_created
+  echo "admin user '${CRAYVCS_USER}' created" >> $LOG_FILE
+  touch ${DATA_MOUNT}/gitea/crayvcs_user_created
+  echo `date` >> ${DATA_MOUNT}/gitea/crayvcs_user_created
 else
-  echo "User creation failed OR admin user '${CRAYVCS_USER}' has previously been created" >> /data/gitea/setup
+  echo "User creation failed OR admin user '${CRAYVCS_USER}' has previously been created" >> $LOG_FILE
 fi
